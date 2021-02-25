b567309051ae13c7d683a7e226154065f609f103
Andre <apfelsinemail@gmx.de>
2021-02-25 22:59:36 +0100

    First UI Draft for Ball

---
+++ src/main/kotlin/BallGame.kt
@@ -0,0 +1,19 @@
+class BallGame {
+
+    var arc1pos = -1
+    var arc2pos = -1
+    var arc3pos = -1
+
+    val arc1length = 8
+    val arc2length = 10
+    val arc3length = 12
+
+    enum class Direction {
+        LEFT, RIGHT, STOP
+    }
+
+    var dir1 = Direction.STOP
+    var dir2 = Direction.STOP
+    var dir3 = Direction.STOP
+
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,20 +1,137 @@
 import androidx.compose.desktop.Window
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 fun main() = Window {
     var text by remember { mutableStateOf("Hello, World!") }
+    var text3 by remember { mutableStateOf("Hello2") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+        Column {
+            /*Row {
+                Button(onClick = {
+                    text = "Hello, Desktop!"
+                }) {
+                    Text(text)
+                }
+
+                Button(onClick = {
+                    text3 = "Hallo"
+                }) {
+                    Text(text3)
+                }
+
+
+            }*/
+
+            ball()
+
+            Row {
+                Button(onClick = { leftClick() }) {
+                    Text("<")
+                }
+                Button(onClick = { rightClick() }) {
+                    Text(">")
+                }
+            }
         }
+
+
     }
+}
+
+fun leftClick() {
+    println("left")
+}
+
+fun rightClick() {
+    println("right")
+}
+
+
+@Composable
+fun hand(active: Boolean) {
+    Box(
+        modifier = Modifier
+            .height(100.dp)
+            .width(50.dp)
+            .padding(10.dp)
+            .background(color = if (active) Color.Gray else Color.Black)
+    )
+}
+
+@Composable
+fun ballPlaceholder(active: Boolean) {
+    Box(
+        modifier = Modifier
+            .height(100.dp)
+            .width(100.dp)
+            .padding(10.dp)
+            .background(color = if (active) Color.Blue else Color.Gray)
+    )
+}
+
+/**
+ * An arc, that a ball travels on
+ */
+@Composable
+fun arc(size: Int, ballPos: Int?) {
+    Row(
+    ) {
+        hand(active = false)
+        for (i in 0..size) {
+            ballPlaceholder(active = i == ballPos)
+        }
+        hand(active = false)
+    }
+}
+
+@Composable
+fun ball() {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.LightGray)
+    ){
+
+        arc(size = 12, ballPos = 0)
+        arc(size = 10, ballPos = null)
+        arc(size = 8, ballPos = 3)
+
+        /*Column {
+
+            }
+
+
+        }
+        Column {
+
+        }
+        Column {
+
+        }
+        Column {
+
+        }
+        Column {
+
+        }
+        Column {
+
+        }*/
+    }
+}
+
+@Composable
+fun previewBall() {
+    ball()
 }
